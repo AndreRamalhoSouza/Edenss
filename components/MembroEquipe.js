7ba23db8d2058e39ad03620ae11ad1d26cb4c58c
Andre Ramalho <ramalhompi@gmail.com>
2025-06-23 23:13:58 -0300

    k

--- components/MembroEquipe.js
+++ components/MembroEquipe.js
@@ -9,12 +9,7 @@ function MembroEquipe() {
     imagem: "/images/Samuel.png", 
     bio: "Arquiteto urbanista desde 2014, formado pela PUC-Campinas, com especializações em Administração e Gestão Empresarial pela FGV. Com experiência em incorporação imobiliária, gerenciamento de obras, projetos arquitetônicos, paisagísticos e de design, busca criar espaços que vão além da funcionalidade. Sócio de Edenss, lidera uma equipe focada em desenvolver hotéis e parques acolhedores, unindo sustentabilidade, inovação e bem-estar. Acredita no poder transformador da arquitetura para criar experiências memoráveis e um mundo melhor."
   },
-    {
-    nome: "Susi",
-    cargo: "Arquiteta Especialista em Hotelaria e Entretenimento",
-    imagem: "/images/Susi.png", 
-    bio: "Formada em Arquitetura e Urbanismo, com pós-graduação em Arquitetura Hoteleira e cursando MBA em Arquitetura Hoteleira, Susi possui ampla experiência em projetos de atrações aquáticas, parques e destinos de lazer familiar. Seu conhecimento abrange o design de produtos para hotéis, pousadas e parques aquáticos, enriquecido por viagens ao redor do mundo, que lhe deram uma visão inovadora do setor. Na Edenss, sua missão é criar experiências únicas e inesquecíveis, projetando ambientes que encantam, conectam e transformam sonhos em realidade.",
-  },
+   
   ];
 
   return (
