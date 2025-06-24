2ecd71128ac83534cce9d86c6a003f5f01bf0c60
Andre Ramalho <ramalhompi@gmail.com>
2025-06-23 22:52:57 -0300

    f

--- components/MembroEquipe.js
+++ components/MembroEquipe.js
@@ -3,18 +3,18 @@ import styles from '../styles/MembroEquipe.module.css';
 
 function MembroEquipe() {
   const membros = [
+   {
+    nome: "Samuel",
+    cargo: "Arquiteto, Gestor e Criador de Espaços Extraordinários",
+    imagem: "/images/Samuel.png", 
+    bio: "Arquiteto urbanista desde 2014, formado pela PUC-Campinas, com especializações em Administração e Gestão Empresarial pela FGV. Com experiência em incorporação imobiliária, gerenciamento de obras, projetos arquitetônicos, paisagísticos e de design, busca criar espaços que vão além da funcionalidade. Sócio de Edenss, lidera uma equipe focada em desenvolver hotéis e parques acolhedores, unindo sustentabilidade, inovação e bem-estar. Acredita no poder transformador da arquitetura para criar experiências memoráveis e um mundo melhor."
+  },
     {
-      nome: 'Samuel',
-      imagem: '/imagens/samuel.jpg', // ajuste o caminho conforme seu projeto
-      cargo: 'Desenvolvedor Front-End',
-      bio: 'Apaixonado por interfaces limpas, acessíveis e funcionais.',
-    },
-    {
-      nome: 'Susi',
-      imagem: '/imagens/susi.jpg', // ajuste o caminho conforme seu projeto
-      cargo: 'UX Designer',
-      bio: 'Focada em entregar experiências intuitivas e centradas no usuário.',
-    },
+    nome: "Susi",
+    cargo: "Arquiteta Especialista em Hotelaria e Entretenimento",
+    imagem: "/images/Susi.png", 
+    bio: "Formada em Arquitetura e Urbanismo, com pós-graduação em Arquitetura Hoteleira e cursando MBA em Arquitetura Hoteleira, Susi possui ampla experiência em projetos de atrações aquáticas, parques e destinos de lazer familiar. Seu conhecimento abrange o design de produtos para hotéis, pousadas e parques aquáticos, enriquecido por viagens ao redor do mundo, que lhe deram uma visão inovadora do setor. Na Edenss, sua missão é criar experiências únicas e inesquecíveis, projetando ambientes que encantam, conectam e transformam sonhos em realidade.",
+  },
   ];
 
   return (
