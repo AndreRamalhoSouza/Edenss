c6006fb9ddbaaa1fc7f337389d166bd354654351
Andre Ramalho <ramalhompi@gmail.com>
2025-06-23 23:49:23 -0300

    g

--- components/MembroEquipe.js
+++ components/MembroEquipe.js
@@ -1,12 +1,17 @@
-// components/MembroEquipe.js
 import React from 'react';
 import styles from '../styles/MembroEquipe.module.css';
 import { membrosEquipe } from '../data/conteudo';
 
 function MembroEquipe() {
+  // Remove duplicatas pelo nome
+  const membrosUnicos = membrosEquipe.filter(
+    (membro, index, self) =>
+      index === self.findIndex(m => m.nome === membro.nome)
+  );
+
   return (
     <div>
-      {membrosEquipe.map((membro, index) => {
+      {membrosUnicos.map((membro, index) => {
         const nomeCss = membro.nome.toLowerCase();
         const imagemDinamicaClass = `${styles.imagemMembro}-${nomeCss}`;
 
