1cc64c6d7c1db36e3deba8ad7cca76f2d339e722
Andre Ramalho <ramalhompi@gmail.com>
2025-06-23 23:20:29 -0300

    l

--- components/MembroEquipe.js
+++ components/MembroEquipe.js
@@ -17,34 +17,5 @@ function MembroEquipe() {
   },
   ];
 
-  return (
-    <>
-      {membros.map((membro, index) => {
-        const nomeCss = membro.nome.toLowerCase();
-        const imagemDinamicaClass = `${styles.imagemMembro}-${nomeCss}`;
-        const textoDinamicaClass = `${styles.textoMembro}-${nomeCss}`;
-        const membroDetalheDinamicaClass = `${styles.membroDetalhe}-${nomeCss}`;
-
-        return (
-          <div
-            key={index}
-            className={`${styles.membroDetalhe} ${membroDetalheDinamicaClass}`}
-          >
-            <img
-              src={membro.imagem}
-              alt={membro.nome}
-              className={`${styles.imagemMembro} ${imagemDinamicaClass}`}
-            />
-            <div className={`${styles.textoMembro} ${textoDinamicaClass}`}>
-              <h3>{membro.nome}</h3>
-              <h4>{membro.cargo}</h4>
-              <p>{membro.bio}</p>
-            </div>
-          </div>
-        );
-      })}
-    </>
-  );
 }
-
 export default MembroEquipe;
