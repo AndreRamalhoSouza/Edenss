b73b8ec6fc7528c9977e153e33fabca7b8931b56
Andre Ramalho <ramalhompi@gmail.com>
2025-06-25 22:32:27 -0300

    resp 2

--- components/Cabecalho.js
+++ components/Cabecalho.js
@@ -1,8 +1,29 @@
 // components/Cabecalho.js
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from '../styles/Cabecalho.module.css';
 
 function Cabecalho() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1023 && isOpen) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isOpen]);
+
   return (
     <header className={styles.cabecalho}>
       <div className={styles.logoContainer}>
@@ -14,37 +35,48 @@ function Cabecalho() {
         <p className={styles.slogan}>Projetando experiências</p>
       </div>
 
-      <nav className={styles.nav}>
+      <div
+        className={`${styles.hamburgerIcon} ${isOpen ? styles.open : ''}`}
+        onClick={toggleMenu}
+      >
+        <div className={styles.bar1}></div>
+        <div className={styles.bar2}></div>
+        <div className={styles.bar3}></div>
+      </div>
+
+      <nav className={`${styles.nav} ${isOpen ? styles.open : ''}`}>
         <ul>
           <li>
-            <Link href="/servicos">
+            <Link href="/servicos" onClick={toggleMenu}>
               Serviços
             </Link>
           </li>
           <li>
-            <Link href="/portfolio">
+            <Link href="/portfolio" onClick={toggleMenu}>
               Projetos
             </Link>
           </li>
-          {/* Item "Nossa História" e Lupa no mesmo <li>, mas como elementos irmãos */}
           <li className={styles.nossaHistoriaItem}>
-            <Link href="/sobre">
+            <Link href="/sobre" onClick={toggleMenu}>
               Nossa História
             </Link>
-            {/* --- LUPA CRIADA COM CSS AQUI --- */}
-            <span className={styles.cssLupaContainer} /* onClick opcional aqui */>
+            <span className={styles.cssLupaContainer}>
               <div className={styles.lupaCircle}></div>
               <div className={styles.lupaHandle}></div>
             </span>
-            {/* --- FIM DA LUPA CRIADA COM CSS --- */}
+          </li>
+          {/* Este é o único item de contato */}
+          <li className={styles.contatoMenuItem}> {/* Adicionei uma nova classe para estilizar este LI */}
+            <Link
+              href="/contato"
+              className={styles.botaoContato}
+              onClick={toggleMenu}
+            >
+              CONTATO
+            </Link>
           </li>
         </ul>
       </nav>
-      <div className={styles.botaoContato}>
-        <Link href="/contato">
-          CONTATO
-        </Link>
-      </div>
     </header>
   );
 }
