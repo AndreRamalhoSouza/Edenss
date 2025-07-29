ac1f26a0a85bd611e276effd0c48c49aba05fa85
Andre Ramalho <ramalhompi@gmail.com>
2025-07-28 21:55:26 -0300

    f

--- components/MembroEquipe.js
+++ components/MembroEquipe.js
@@ -20,7 +20,14 @@ function MembroEquipe({ membro, reverseOrder }) {
       />
       <div className={`${styles.textoMembro} ${textoDinamicaClass}`}>
         <h3>{membro.nome}</h3>
-        <h4>{membro.cargo}</h4>
+        <h4>
+          {membro.cargo.split('\n').map((linha, index) => (
+            <React.Fragment key={index}>
+              {linha}
+              <br />
+            </React.Fragment>
+          ))}
+        </h4>
         <p>{membro.bio}</p>
       </div>
     </div>
