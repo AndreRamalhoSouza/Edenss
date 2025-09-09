1dd330571ddf4bc7ed7ee15e457875be565b8ccc
Andre <ramalhompi@gmail.com>
2025-09-09 10:33:07 -0300

    maisc

--- data/projects.js
+++ data/projects.js
@@ -2,8 +2,8 @@
 
 export const projects = [
   {
-    id: 'Retrofit-hotel-golden',
-    name: 'Retrofit hotel golden',
+    id: 'Retrofit-Hotel-Golden',
+    name: 'Retrofit Hotel Golden',
     mainImage: '/images/golden.jpeg',
     description: 'Fizemos alguns projetos de retrofit para o hotel , dentre eles a nova piscina infantil e o bar , criando um lounge e um deck de madeira … lugar mais amplo para desfrutar a vista desse lugar incrível.',
     detailImages: [
@@ -14,8 +14,8 @@ export const projects = [
     ],
   },
   {
-    id: 'Retrofit-hotel-Thermas',
-    name: 'Retrofit hotel thermas',
+    id: 'Retrofit-Hotel-Thermas',
+    name: 'Retrofit Hotel Thermas',
     mainImage: '/images/Therm.jpeg',
     description: 'Retrofit da piscina infantil e adulta  , criando espaços com espreguiçadeiras para apreciar o sol enquanto se refresca nas piscinas, e muito paisagismo para conectar com todo o entorno',
     detailImages: [
@@ -27,7 +27,7 @@ export const projects = [
   },
   {
     id: 'Vale-Suiço',
-    name: 'Vale suíço ',
+    name: 'Vale Suíço ',
     mainImage: '/images/sui.jpeg',
     description: 'Grande desafio pois tínhamos que projetar um parque aquático em um hotel, que não ficasse com cara de parque mais sim uma área cheia de atrações como piscinas outdoor e indoor com brinquedos aquáticos , rio lento e piscina de ondas. Projeto ficou lindo.',
     detailImages: [
@@ -38,8 +38,8 @@ export const projects = [
     ],
   },
   {
-    id: 'Vilarejo-fazenda',
-    name: 'Vilarejo fazenda',
+    id: 'Vilarejo-Fazenda',
+    name: 'Vilarejo Fazenda',
     mainImage: '/images/vil.jpeg',
     description: 'Projeto de uma area indoor que fosse multiuso, ou seja , com piscina com. Brinquedo aquático e piscina spa para toda a família , priorizamos a vista incrível do lugar , criando uma piscina outdoor com borda infinita e toda a fachada em vidro ! ',
     detailImages: [
@@ -50,8 +50,8 @@ export const projects = [
     ],
   },
   {
-    id: 'Vilarejo-praia',
-    name: 'Vilarejo praia ',
+    id: 'Vilarejo-Praia',
+    name: 'Vilarejo Praia ',
     mainImage: '/images/praia.png',
     description: 'A ideia era conectar o espaço com a vista incrível do mar, o hotel não havia explorado essa ponto ainda , e assim priorizamos a vista como ponto de partida para o projeto assim criamos uma piscina de ofurôs e borda infinita voltada para o mar, além de uma piscina com brinquedo aquático. ',
     detailImages: [
@@ -62,8 +62,8 @@ export const projects = [
     ],
   },
   {
-    id: 'Vale-encantado',
-    name: 'Vale encantado',
+    id: 'Vale-Encantado',
+    name: 'Vale Encantado',
     mainImage: '/images/vale 1.jpeg',
     description: 'A ideia era conectar os espaços criando uma nova entrada para o parque mas preservando a estrutura do antigo restaurante, pensando nisso trouxemos uma cobertura bem diferente porém que integrasse as 2 arquiteturas , além de um espaço com piscina e brinquedo aquáticos.',
     detailImages: [
@@ -74,8 +74,8 @@ export const projects = [
     ],
   },
   {
-    id: 'Hotel-porto-fazenda',
-    name: 'Hotel porto fazenda ',
+    id: 'Hotel-Porto-Fazenda',
+    name: 'Hotel Porto Fazenda ',
     mainImage: '/images/faz 1.jpeg',
     description: 'Hotel com uma vista linda para a represa mas com ares de campo, tudo bem pensado para integrar água e terra. ',
     detailImages: [
@@ -90,8 +90,8 @@ export const projects = [
     ],
   },
   {
-    id: 'Hotel-aeroporto',
-    name: 'Hotel aeroporto ',
+    id: 'Hotel-Aeroporto',
+    name: 'Hotel Aeroporto ',
     mainImage: '/images/porto 1.jpeg',
     description: 'Esse parque indoor foi projetado ao lado do aeroporto, criando uma atmosfera temática, com mtas atrações, conta com piscinas de ondas, adulta e infantil e ainda mtos  espaços de descanso.',
     detailImages: [
@@ -106,7 +106,7 @@ export const projects = [
   
   {
     id: 'Hotel-Vale-da-Mantiqueira',
-    name: 'Hotel vale da mantiqueira',
+    name: 'Hotel Vale da Mantiqueira',
     mainImage: '/images/mant.jpg',
     description: 'O partido do projeto, surgiu com a ideia do cliente de ter um parque aquático, dentro do hotel existente,inspirado em um Resort de Punta Cana, buscamos reunir neste parque uma pequena piscina de ondas, uma piscina com rio lento e toboáguas.Componto a área externa, temos os bangalôs para os hospedes, gêiser e a caravela que é a atração central do parque. Ao lado da pista de dança, utilizada para as atrações noturnas do hotel, temos um espaço para lareira, afinal na serra da Mantiqueira é sempre bom uma fogueira para se aquecer. O Edifício abriga um lindo bar, pista de dança, pista de boliche e uma piscina coberta.',
 
@@ -124,8 +124,8 @@ export const projects = [
 
 
   {
-    id: 'Pousada-charme',
-    name: 'Pousada charme',
+    id: 'Pousada-Charme',
+    name: 'Pousada Charme',
     mainImage: '/images/mant s.jpeg',
     description: 'Esse hotel tem uma localização  privilegiada em uma área  em volta da natureza e próximo ao um parque aquático conta com piscina de borda infinita e suspensa,integrando o restaurante através de decks em níveis diferentes com a área das piscina adulta e infantil. Perfeito para relaxamento e diversão para toda a família ',
 
@@ -145,7 +145,7 @@ export const projects = [
   
 
   {
-    id: 'Monreale-park',
+    id: 'Monreale-Park',
     name: 'Monreale',
     mainImage: '/images/mon.png',
     description: 'O hotel prioriza a diversão em família , pensando nisso criamos piscinas com atrações para todas as idades, desde de piscina com spas,e piscinas infantis com brinquedos aquáticos.E para os mais radicais ainda conta com uma torre com 3 Toboáguas. Além disso criamos uma área indoor com vários brinquedos aquáticos ',
@@ -165,8 +165,8 @@ export const projects = [
 
 
   {
-    id: 'Hot-world',
-    name: 'Hot world',
+    id: 'Hot-World',
+    name: 'Hot World',
     mainImage: '/images/hot.jpg',
     description: 'Fundado na década de 90, o Thermas Water Park de Águas de Lindóia, não tinha passado por nenhuma revitalização desde então. Ao ser adquirido por um grupo em 2018, a primeira iniciativa foi a contratação de um Masterplan para o parque.Surgiu então o Thermas Hot World, um parque aquático completo, inspirado nos parques americanos, com o que há de moderno neste segmento.',
 
@@ -188,8 +188,8 @@ export const projects = [
 
 
  {
-    id: 'Village-inn',
-    name: 'Village inn',
+    id: 'Village-Inn',
+    name: 'Village Inn',
     mainImage: '/images/dan.jpeg',
     description: 'Esse hotel quisemos trazer uma ideia de resort urbano,  projetadospara oferecer aos hóspedes uma experiência de férias completa, com uma ampla gama de comodidades e atividades para toda a família. ',
 
@@ -207,7 +207,7 @@ export const projects = [
 
  {
     id: 'Blue-Beach',
-    name: 'Blue beach',
+    name: 'Blue Beach',
     mainImage: '/images/bl.jpeg',
     description: 'Aguardando Texto... ',
 
@@ -223,8 +223,8 @@ export const projects = [
   },
 
  {
-    id: 'Pousada-dos-sonhos',
-    name: 'Pousada dos sonhos',
+    id: 'Pousada-dos-Sonhos',
+    name: 'Pousada dos Sonhos',
     mainImage: '/images/po s.jpeg',
     description: 'Aguardando Texto... ',
 
@@ -244,8 +244,8 @@ export const projects = [
 
 
  {
-    id: 'Resort-urbano',
-    name: 'Resort urbano',
+    id: 'Resort-Urbano',
+    name: 'Resort Urbano',
     mainImage: '/images/re.jpeg',
     description: 'Aguardando Texto... ',
 
@@ -262,7 +262,7 @@ export const projects = [
 
    {
     id: 'Sesi-Go',
-    name: 'Sesi go',
+    name: 'Sesi Go',
     mainImage: '/images/se p.jpeg',
     description: 'Aguardando Texto... ',
 
@@ -279,7 +279,7 @@ export const projects = [
 
  {
     id: 'Villa-Di-Mantova',
-    name: 'Villa di mantova',
+    name: 'Villa Di Mantova',
     mainImage: '/images/vi.jpeg',
     description: 'Aguardando Texto... ',
 
@@ -293,15 +293,6 @@ export const projects = [
 
     ],
   },
-
-
-
-
-
-
-
-
-
 
 
 
